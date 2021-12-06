0dc4a0f240a2b61556ba828dd19d7dbb4538c709
evgenii.fesenko <evgenii.fesenko@eosda.com>
2021-12-06 23:55:44 +0200

    [main] add lesson 1 home_work

---
+++ lesson1/home_work/print_info.py
@@ -0,0 +1,14 @@
+"""
+    Объявить переменные name, age, city,
+    присвоив им значения с помощью input().
+
+    Вывести на экран текст типа:
+
+    Max from Kiev
+    He is 21
+"""
+name = input('Enter name: ')
+age = input('Enter age: ')
+city = input('Enter city: ')
+
+print(f'{name} from {city}\nHe is {age}')
