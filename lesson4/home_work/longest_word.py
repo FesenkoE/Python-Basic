2a192bcfcc591d62a88420e67fff91c95989c2dc
evgenii.fesenko <evgenii.fesenko@eosda.com>
2021-12-13 01:18:48 +0200

    [main] add lesson 4 home_work 2 task

--- lesson4/home_work/longest_word.py
+++ lesson4/home_work/longest_word.py
@@ -5,7 +5,25 @@
     2. Вывести самое длинное слово и его длину.
 """
 
-# Ниже описаны некоторые варианты решения задачи.
-
-# 1 вариант
 string = input("Enter a string: ")
+counter = 0
+word = ''
+longest_word = ''
+
+for char in string:
+    if char.isalpha():
+        word += char
+    else:
+        if len(word) > len(longest_word):
+            longest_word = word
+        counter += 1
+        word = ''
+else:
+    if word:
+        counter += 1
+    if len(word) > len(longest_word):
+        longest_word = word
+
+print(f'Len of string is: {counter}')
+print(f'longest word is: {longest_word}')
+
