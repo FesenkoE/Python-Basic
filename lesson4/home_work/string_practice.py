28cbe0ffc4ceaa297a4f678a8df737e7dbadfa43
evgenii.fesenko <evgenii.fesenko@eosda.com>
2022-06-07 09:40:57 +0300

    hw-4 string practice

--- lesson4/home_work/string_practice.py
+++ lesson4/home_work/string_practice.py
@@ -5,15 +5,24 @@ string = 'Lorem, Ipsum, is, simply, dummy, text, of, the, printing, industry.'
    Результат: 'Lorem Ipsum is simply dummy text of the printing industry.'
 """
 
+string = string.replace(', ', ' ')
+print(f'String: {string}')
+
 """
 2. Найти индекс самой последней буквы 's' в строке.
    Результат: 53
 """
 
+last_s_idx = string.rindex('s')
+print(f'Last "S" index: {last_s_idx}')
+
 """
 3. Найти количество букв 'i' в строке (регистр не имеет значения).
    Результат: 6
 """
+
+i_counter = string.lower().count('i')
+print(f'Count "i": {i_counter}')
 
 """
 4. Найти и вывести срез строки.
@@ -21,9 +30,17 @@ string = 'Lorem, Ipsum, is, simply, dummy, text, of, the, printing, industry.'
    (используйте методы find или index для получения индексов)
 """
 
+idx = string.find('simply dummy text')
+slicing = string[idx:idx + len('simply dummy text')]
+print(f'Slicing: {slicing}')
+
 """
 5. Продублируйте первую половину строки 3 раза и склейте с второй половиной
    и выведите на экран.
    Результат: 'Lorem Ipsum is simply dummy tLorem Ipsum is simply dummy tLorem Ipsum is simply dummy text of the
    printing industry.'
 """
+
+first_part = (string[:len(string) // 2]) * 3
+second_part = string[len(string) // 2:]
+print(f'Result: {first_part + second_part} ')
