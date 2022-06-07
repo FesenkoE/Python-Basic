ca7ee770829b59221b24a241630f8917c0132cdd
evgenii.fesenko <evgenii.fesenko@eosda.com>
2022-06-07 09:39:55 +0300

    hw-4 longest word

--- lesson4/home_work/longest_word.py
+++ lesson4/home_work/longest_word.py
@@ -5,3 +5,23 @@
     2. Вывести самое длинное слово и его длину.
 """
 
+# Lorem Ipsum is simply dummy text of the printing industry.
+
+str_ = input('Enter string: ')
+word_counter = 0
+word = 0
+longest_word = 0
+
+if str_:
+    word_counter = 1
+
+    for char in str_:
+        if not char.isalnum():
+            word_counter += 1
+            word = 0
+        else:
+            word += 1
+            longest_word = word if word > longest_word else longest_word
+
+print(f'Words amount: {word_counter}')
+print(f'Longest word: {longest_word}')
