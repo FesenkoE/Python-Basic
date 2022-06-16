f9462285e5a9f7f819fa0dee11385a1777cc8d7e
evgenii.fesenko <evgenii.fesenko@eosda.com>
2022-06-16 21:49:33 +0300

    hw-5 Example Gen

--- lesson5/home_work/example_gen.py
+++ lesson5/home_work/example_gen.py
@@ -6,3 +6,18 @@
     * можно делать выборку в цикле и конкатенировать все в одну строку,
     если нужно больше, чем 1 символ
 """
+
+import string
+import random
+
+
+def main():
+    special_string = string.digits + string.punctuation
+    special_char = random.choice(special_string)
+
+    print(f'Special Char: {special_char}')
+
+
+if __name__ == "__main__":
+    main()
+
